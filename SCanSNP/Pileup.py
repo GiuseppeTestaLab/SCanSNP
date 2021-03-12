acda2cf7b2a8c0e20ac27f56f0e09c8899595a81
castaway1990 <davide.castaldi@ieo.it>
2021-03-12 07:45:36 +0100

    Reduced memory load in multiprocessing

--- SCanSNP/Pileup.py
+++ SCanSNP/Pileup.py
@@ -10,49 +10,74 @@ import numpy as np
 import pandas as pd
 import itertools
 from scipy.sparse import csr_matrix
+import scipy.sparse
 
 
 
 
 
-def ReadCounter(chunkIdx, bamFile, barcodeList, GenotypeChunkList, bannedFlag=3844, mapQuality=2, baseQuality=20 , readLength=30):
+
+
+def ReadCounter(chunkIdx, bamFile, barcodeList, GenotypeChunkList):
 	bam=pysam.AlignmentFile(bamFile, "rb")
 	BarcodeSet=set(barcodeList)
-	readList=[]
+	readList = []
+	SparseD = {"sparse_Ref" : csr_matrix((0, len(barcodeList))), "sparse_Alt":csr_matrix((0, len(barcodeList))), "Locus" : pd.Series()  }
+	lastPos = chunkIdx[-1]
 	for indexPos in chunkIdx:
+		
 		locus = GenotypeChunkList[indexPos]
-		for read in bam.fetch(locus[0], locus[1]-1, locus[1]):
-		#Check for position coverage
+		
+		readList=Pileupper(bam, locus, lastPos,BarcodeSet, readList)
+		
+		if sys.getsizeof(readList) >= 30000000 or indexPos == lastPos:
 			try:
-				CB=read.get_tag("CB")
+				sparse_Ref_TMP,sparse_Alt_TMP,locus_TMP = DFMaker(readList, barcodeList)
 			except:
 				continue
-			if not set([CB]).intersection(BarcodeSet):
-				continue
-			try:
-				position = read.positions.index(int(locus[1]) - 1)
-			except:
-				continue
-			try:
-				UB=read.get_tag("UB")
-			except:
-				continue
-			if read.query_alignment_qualities[position] < 20:
-				continue
-			if read.is_secondary:
-				continue
-			if read.mapq < 2:
-				continue
-			if read.rlen < 30:
-				continue
-			if read.flag == 3844:
-				continue
-			redBase = read.query_alignment_sequence[position]
-			if ( redBase == locus[2] or redBase == locus[3]):
-				readList.append([locus[0]+ "_"+str(locus[1]), CB, redBase, locus[2], locus[3] ])
+			SparseD["sparse_Ref"] = scipy.sparse.vstack((SparseD["sparse_Ref"],sparse_Ref_TMP))
+			SparseD["sparse_Alt"] = scipy.sparse.vstack((SparseD["sparse_Alt"],sparse_Alt_TMP))
+			SparseD["Locus"] = SparseD["Locus"].append(locus_TMP)
+			readList = []
 	bam.close()
 	print("Chunk "+ str(chunkIdx) + " completed")
+	return SparseD
+
+
+
+def Pileupper(bam, locus, lastPos, BarcodeSet, readList, bannedFlag=3844, mapQuality=2, baseQuality=20 , readLength=30):
+	##!!! Accessing readList "fake global" because of diverse GIL spawned for childs<<<<
+	for read in bam.fetch(locus[0], locus[1]-1, locus[1]):
+	#Check for position coverage
+		try:
+			CB=read.get_tag("CB")
+		except:
+			continue
+		if not set([CB]).intersection(BarcodeSet):
+			continue
+		try:
+			position = read.positions.index(int(locus[1]) - 1)
+		except:
+			continue
+		try:
+			UB=read.get_tag("UB")
+		except:
+			continue
+		if read.query_alignment_qualities[position] < 20:
+			continue
+		if read.is_secondary:
+			continue
+		if read.mapq < 2:
+			continue
+		if read.rlen < 30:
+			continue
+		if read.flag == 3844:
+			continue
+		redBase = read.query_alignment_sequence[position]
+		if ( redBase == locus[2] or redBase == locus[3]):
+			readList.append([locus[0]+ "_"+str(locus[1]), CB, redBase, locus[2], locus[3] ])
 	return readList
+
 
 
 
@@ -67,7 +92,6 @@ def DFMaker(readList, barcodeList):
 	Barcode_c = pd.CategoricalDtype(sorted(list(barcodeList)), ordered=True)
 	#Storing Ref Counts
 	#Storing Ref Counts
-	print("Storing RefReads...")
 	RefReads=RefReads.groupby(["Pos","Barcode"], as_index = False)
 	RefReads=RefReads.size()
 	RefReads.rename(columns={'size':'Counts'}, inplace=True)
@@ -75,10 +99,8 @@ def DFMaker(readList, barcodeList):
 	Pos_Ref = RefReads.Pos.astype(Pos_c).cat.codes
 	Barcode_Ref = RefReads.Barcode.astype(Barcode_c).cat.codes
 	sparse_Ref = csr_matrix((RefReads["Counts"], (Pos_Ref, Barcode_Ref)), shape=(len(uniqueLoci), len(barcodeList)))
-	print("RefReads stored")
 	#Storing Alt Counts
 	#Storing Alt Counts
-	print("Storing AltReads...")
 	AltReads=AltReads.groupby(["Pos","Barcode"], as_index = False)
 	AltReads=AltReads.size()
 	AltReads.rename(columns={'size':'Counts'}, inplace=True)
@@ -86,5 +108,49 @@ def DFMaker(readList, barcodeList):
 	Pos_Alt = AltReads.Pos.astype(Pos_c).cat.codes
 	Barcode_Alt = AltReads.Barcode.astype(Barcode_c).cat.codes
 	sparse_Alt = csr_matrix((AltReads["Counts"], (Pos_Alt, Barcode_Alt)), shape=(len(uniqueLoci), len(barcodeList)))
-	print("RefReads and AltReads stored")
-	return sparse_Ref,sparse_Alt,pd.Series(sorted(uniqueLoci)),pd.Series(sorted(list(barcodeList)))
+	return sparse_Ref,sparse_Alt,pd.Series(sorted(uniqueLoci))
+
+
+
+
+
+# 
+# 
+# def OLLDDDDDDREF(chunkIdx, bamFile, barcodeList, GenotypeChunkList, bannedFlag=3844, mapQuality=2, baseQuality=20 , readLength=30):
+# 	bam=pysam.AlignmentFile(bamFile, "rb")
+# 	BarcodeSet=set(barcodeList)
+# 	readList=[]
+# 	for indexPos in chunkIdx:
+# 		locus = GenotypeChunkList[indexPos]
+# 		for read in bam.fetch(locus[0], locus[1]-1, locus[1]):
+# 		#Check for position coverage
+# 			try:
+# 				CB=read.get_tag("CB")
+# 			except:
+# 				continue
+# 			if not set([CB]).intersection(BarcodeSet):
+# 				continue
+# 			try:
+# 				position = read.positions.index(int(locus[1]) - 1)
+# 			except:
+# 				continue
+# 			try:
+# 				UB=read.get_tag("UB")
+# 			except:
+# 				continue
+# 			if read.query_alignment_qualities[position] < 20:
+# 				continue
+# 			if read.is_secondary:
+# 				continue
+# 			if read.mapq < 2:
+# 				continue
+# 			if read.rlen < 30:
+# 				continue
+# 			if read.flag == 3844:
+# 				continue
+# 			redBase = read.query_alignment_sequence[position]
+# 			if ( redBase == locus[2] or redBase == locus[3]):
+# 				readList.append([locus[0]+ "_"+str(locus[1]), CB, redBase, locus[2], locus[3] ])
+# 	bam.close()
+# 	print("Chunk "+ str(chunkIdx) + " completed")
+# 	return readList
